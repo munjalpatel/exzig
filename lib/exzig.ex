d7c384a6738569771e3275ded7621bce654b74ad
Munjal A Patel <munjalapatel@gmail.com>
2023-09-14 12:58:11 -0700

    initial commit

---
+++ lib/exzig.ex
@@ -0,0 +1,18 @@
+defmodule Exzig do
+  @moduledoc """
+  Documentation for `Exzig`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exzig.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exzig_test.exs
@@ -0,0 +1,8 @@
+defmodule ExzigTest do
+  use ExUnit.Case
+  doctest Exzig
+
+  test "greets the world" do
+    assert Exzig.hello() == :world
+  end
+end
